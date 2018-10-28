ec80789a9f713f9f4a7ab4292ab8983e1681aeb3
Vaibhav Jain <vaibhav.s.jain@gmail.com>
2018-10-28 18:38:26 +1100

    changes done for practice

--- src/main/java/com/DevOps/Training/TestOne_KP.java
+++ src/main/java/com/DevOps/Training/TestOne_KP.java
@@ -4,7 +4,7 @@ public class TestOne_KP {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to Devops training makkaLe");
+		System.out.println("Welcome to Devops training nanna muddu makkale");
 	}
 
 }
