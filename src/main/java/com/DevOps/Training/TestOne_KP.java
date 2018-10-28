95400e86434dbd086c24a136e96b11c9fbecb0b7
Vaibhav Jain <vaibhav.s.jain@gmail.com>
2018-10-28 18:03:17 +1100

    adding my code to git hub

---
+++ src/main/java/com/DevOps/Training/TestOne_KP.java
@@ -0,0 +1,10 @@
+package com.DevOps.Training;
+
+public class TestOne_KP {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Devops training");
+	}
+
+}
